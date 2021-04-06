1beed60c4c0c394bb46bc661621bb639ebc1ea26
Hardikkr <hardik.kumar18feb@gmail.com>
2021-04-07 00:15:49 +0530

    husky command designed

--- docs.md
+++ docs.md
@@ -5,3 +5,8 @@ Event is a piece of code that is triggered when the bot detects something has ha
 
 > @Husky.command() <br>
 Command is a piece of coed which is triggered when a user tells the bot to trigger
+
+
+## Another way of getting bot's information
+> print("{0.user}".format(Husky))
+> print("{0.user.avatar_url}".format(Husky))
--- husky.py
+++ husky.py
@@ -1,40 +1,82 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 import reading_from_gsheets
 import writing_in_gsheets
 from Private import private_keys
 
+
 Husky = commands.Bot(command_prefix="*")
 
-#* Basic details
+
+#? Husky details
+husky_username = ""
+husky_avatar_url = ""
+
+
+#? Basic details
 username = ""
 name = ""
 email = ""
 age = ""
 city = ""
 
+
 @Husky.event
 async def on_ready():
-    print("{0.user} is ready".format(Husky))
+    husky_username = Husky.user
+    husky_avatar_url = Husky.user.avatar_url
+    print(husky_username)
+    print(husky_avatar_url)
+
 
 @Husky.command()
 async def husky(ctx):
-    await ctx.send("Please provide your details like this:\n*register name | email | age | city")
+    username = str(ctx.author)
+    avatar_url = str(ctx.author.avatar_url)
+    print("\nusername: ", username, "\navatar_url: ", avatar_url)
+
+    embed = discord.Embed(
+        title = "Woof! Hii there, I'm Husky",
+        description = '''
+            Hello **''' + username + '''**, I'm here to help you out with Registration Process.
+
+            I'd need some basic information from you to know you more and register you in our event. This includes:
+             > 1. Your full name
+             > 2. Your professional email address
+             > 3. Your valid phone number
+             > 4. Your age
+             > 5. City from where you're hacking
+             > 6. Country you are living in
+
+            ***register 1 | 2 | 3 | 4 | 5 | 6**  
+            Please follow this format and provide us the above details.
+        ''',
+        colour = 10181046 #? purple
+    )
+    embed.set_thumbnail(url = avatar_url)
+    embed.set_footer(
+        icon_url = husky_avatar_url,
+        text = "Your data will be kept private"
+    )
+    await ctx.send(embed = embed)
+
 
 @Husky.command()
 async def register(ctx, *, text): #? "*" will take the full sentence
-    username = str(ctx.author)
-    print(username, type(username), "\n")
+    timestamp = datetime.now()
+    username = str(ctx.author.username)
 
     user = text.split("|")
-    for i in range(4):
+    for i in range(len(user)):
         user[i] = user[i].strip()
-    print(user, "\n")
 
     user.append(username)
+    user.append(timestamp)
     print(user)
 
     writing_in_gsheets.add_a_person(user)
     await ctx.send("Registration completed successfully🐶")
+
 
 Husky.run(private_keys.discord_token)
